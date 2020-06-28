6e4ca5f3cdda87f21ca121f3bd128abc6c274217
jq2st <igaaaaan7@gmail.com>
2020-06-28 07:08:22 +0300

    c2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
+import { CatalogCategoriesComponent } from './catalog-page/catalog-categories/catalog-categories.component';
+import { CatalogComponent } from './catalog-page/catalog/catalog.component';
+import { CatalogItemComponent } from './shared/components/catalog-item/catalog-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CatalogPageComponent,
+    CatalogCategoriesComponent,
+    CatalogComponent,
+    CatalogItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/catalog-page/catalog-categories/catalog-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-categories',
+  templateUrl: './catalog-categories.component.html',
+  styleUrls: ['./catalog-categories.component.scss']
+})
+export class CatalogCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
