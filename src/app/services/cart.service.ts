e6f33fea9195e7bdfdafa0db534a2fa0615826c4
jq2st <igaaaaan7@gmail.com>
2020-07-01 08:59:27 +0300

    local

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,17 +12,47 @@ export class CartService {
   addItem(amount, itemId) {
     this.cartMainNum += amount
     localStorage.setItem("cartMainNum", "" + this.cartMainNum)
-    if (localStorage.getItem("cartItemIds").length > 0) {
+
+    if (localStorage.getItem("cartItemIds") != null) {
+      let qurCartIds = localStorage.getItem("cartItemIds")
       let itemIdsArray = []
+      let totalAmount
       localStorage.getItem("cartItemIds").split('___').forEach(i => {
         console.log(i.split('__'))
         itemIdsArray.push(i.split('__'))
       })
 
-      let newV = localStorage.getItem("cartItemIds") + '___'
+      let arethere = 0
+      itemIdsArray.forEach(n => {
+        if (n[0] == itemId) {
+          totalAmount = Number.parseInt(n[1]) + amount
+          arethere++
+        }
+      })
       console.log(itemIdsArray)
-      // let totalAmount = 
-      localStorage.setItem("cartItemIds", "" + itemId + "__" + amount)
+      itemIdsArray = itemIdsArray.filter(n => {
+        return n[0] != itemId
+      })
+
+      let newArr = []
+      itemIdsArray.forEach((n, i) => {
+        newArr[i] = n.join('__')
+      })
+      let qurValues = newArr.join('___')
+
+      if (arethere == 0) {
+        localStorage.setItem("cartItemIds", qurCartIds + '___' + itemId + "__" + amount)
+      }
+      else {
+        localStorage.setItem("cartItemIds", qurValues + '___' + itemId + "__" + totalAmount)
+      }
+      
+
+      console.log(localStorage.getItem("cartItemIds"))
+      console.log(itemIdsArray)
+    }
+    else {
+      localStorage.setItem("cartItemIds", itemId + "__" + amount)
     }
   }
 
